2eb4b023fd0689694f859cf2b15578e1f85329d8
Tim Renken <tim@timrenken.io>
2015-02-27 19:21:33 -0600

    added relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   has_many :posts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   
   validates_presence_of :username
   validates_uniqueness_of :username
@@ -14,4 +22,20 @@ class User < ActiveRecord::Base
   def feed
     Post.where('user_id = ?', id)
   end
+  
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'registrations'}
   root 'pages#index'
 
-  resources :users, only: [:show, :index]
   resources :posts, only: [:create, :destroy]
+  
+  resources :users, only: [:show, :index] do
+    member do
+      get :following, :followers
+    end
+  end
+ 
 end
