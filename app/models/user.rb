2b6ffcf3dc48982b82b816b7a56cb1543d81ed3e
Tim Renken <tim@timrenken.io>
2015-02-28 15:22:46 -0600

    updated followers/following, and fixed Post Feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,10 @@ class User < ActiveRecord::Base
   validates_presence_of :name
   
   def feed
-    Post.where('user_id = ?', id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Post.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
   
   # Follows a user.
